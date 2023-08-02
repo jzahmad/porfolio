5c4ff6d8297a7798278753a1e8ef88361d8b49eb
Jazib Ahmad <jz346475@dal.ca>
2023-08-02 19:26:56 -0300

    final

---
+++ src/Dev/CV.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import './CV.css';
+
+function EducationItem({ institution, duration, courses }) {
+  return (
+    <div className="education-item">
+      <h3>{institution}</h3>
+      <p>{duration}</p>
+      <ul style={{}}>
+        {courses.map((course, index) => (
+          <li key={index}>{course}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function CoopStatus({ status }) {
+  return (
+    <div className="coop-status">
+      <h3>CO-OP/Internship</h3>
+      <p>{status}</p>
+    </div>
+  );
+}
+
+function Skills({ technical, interpersonal }) {
+  return (
+    <div className="skills">
+      <div className="technical-skills">
+        <h3>Technical Skills</h3>
+        <ul>
+          {technical.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="interpersonal-skills">
+        <h3>Interpersonal Skills</h3>
+        <ul>
+          {interpersonal.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function OtherExperience({ title, duration, location, responsibilities }) {
+  return (
+    <div className="other-experience">
+      <h3>{title}</h3>
+      <p>{duration}</p>
+      <p>{location}</p>
+      <ul>
+        {responsibilities.map((responsibility, index) => (
+          <li key={index}>{responsibility}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function CV() {
+  // Education data
+  const educationData = [
+    {
+      institution: 'Bachelor of Computer Science, Saint Mary’s University',
+      duration: '2021 – 2022',
+      courses: ['Intro to Web development', 'Data Structure & Algorithms', 'OOP',],
+    },
+    {
+      institution: 'Bachelor of Computer Science, Dalhousie University',
+      duration: '2022 – 2025',
+      courses: ['Software Development', 'Database Systems', 'System Programming'],
+    },
+  ];
+
+  // Coop status data
+  const coopStatusData = 'Seeking a 4-to-8-month work term in fall 2023';
+
+  // Skills data
+  // Skills data
+  const technicalSkillsData = [
+    'Java, C, JavaScript, HTML, CSS, SQL, Aseba, Python, R',
+    'Unix',
+    'Git, GitHub',
+    'jQuery, Bootstrap, React, Springboot',
+    'IntelliJ, NetBeans, VCS',
+  ];
+
+  const interpersonalSkillsData = [
+    'Time Management', 'Organization', 'Written and Verbal Communication',
+    'Collaboration', 'Dual language (English and Urdu)'
+  ];
+
+  // Other Experience data
+  const otherExperienceData = [
+    {
+      title: 'Bakery Clerk, Atlantic Superstore (Part-Time)',
+      duration: '2021-present',
+      location: 'Halifax, NS',
+      responsibilities: [
+        'Monitored counter and display inventory by replacing stock and requesting new products from the kitchen to keep areas well-stocked for maximum sales.',
+        'Arranged bakery displays to showcase baked goods, cakes, and pies to drive product sales.',
+        'Answered customer questions about products and services, helped locate merchandise, and promoted key items.',
+      ],
+    },
+    {
+      title: 'Security Officer, Shadow Security (On-Call)',
+      duration: '2022 – present',
+      location: 'Halifax, NS',
+      responsibilities: [
+        'Acted quickly during emergency situations to reduce opportunities for damage and injury.',
+        'Protected facility and property guests by regularly circulating premises and monitoring surveillance feeds.',
+        'Conducted regular patrols of key areas to spot and control security concerns.',
+        'Oversaw daily monitoring and patrolled buildings, grounds, and work sites.',
+      ],
+    },
+  ];
+
+  return (
+    <div className="cv-container">
+      <h1>CV</h1>
+      <div className="sections">
+        <div className="left-section">
+          <div className="education-section">
+            <h2>Education</h2>
+            {educationData.map((education, index) => (
+              <EducationItem
+                key={index}
+                institution={education.institution}
+                duration={education.duration}
+                courses={education.courses}
+              />
+            ))}
+          </div>
+          <div className="skills-section">
+            <h2>Skills</h2>
+            <Skills technical={technicalSkillsData} interpersonal={interpersonalSkillsData} />
+          </div>
+        </div>
+        <div className="right-section">
+          <CoopStatus status={coopStatusData} />
+          <div className="other-experience-section">
+            <h2>Other Experience</h2>
+            {otherExperienceData.map((experience, index) => (
+              <OtherExperience
+                key={index}
+                title={experience.title}
+                duration={experience.duration}
+                location={experience.location}
+                responsibilities={experience.responsibilities}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CV;
---
+++ src/Dev/Home.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+import myImage from './prof.jpg';
+import './Home.css';
+
+const name = 'Jazib Ahmad';
+
+function Home() {
+  const [showContent1, setShowContent1] = useState(false);
+  const [showContent2, setShowContent2] = useState(false);
+  const [showContent3, setShowContent3] = useState(false);
+  const phoneNumber = "7824143561";
+  const email = "jazibahmad1010@gmail.com";
+  const linkedInId = "jazib-ahmad-19457a230";
+
+  // Delay time in milliseconds (adjust as needed)
+  const delayTime1 = 3000; // Set the desired delay for content 1
+  const delayTime2 = 4000; // Set the desired delay for content 2
+  const delayTime3 = 5000; // Set the desired delay for content 3
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setShowContent1(true);
+    }, delayTime1);
+
+    const timer2 = setTimeout(() => {
+      setShowContent2(true);
+    }, delayTime2);
+
+    const timer3 = setTimeout(() => {
+      setShowContent3(true);
+    }, delayTime3);
+
+    // Cleanup the timers when the component unmounts
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+      clearTimeout(timer3);
+    };
+  }, []);
+
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${email}`;
+  };
+
+  const handlePhoneClick = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
+  const handleLinkedInClick = (event) => {
+    event.preventDefault();
+    window.open(`https://www.linkedin.com/in/${linkedInId}`, '_blank');
+  };
+
+  return (
+    <div className="screen">
+      <Card className='showCard'>
+        {showContent1 && (
+          <CardContent>
+            <Typography variant="h4">
+              {name} (Student)
+            </Typography>
+          </CardContent>
+        )}
+        {showContent3 && (
+          <CardContent >
+            <Typography variant="h5" gutterBottom>
+              Welcome to my portfolio
+            </Typography>
+            <Typography variant="body1" style={{ lineHeight: '1.6' }}>
+              I am a 2nd-year Computer Science student. My passion lies in AI, Web Applications, and Game Development. I am proficient in a variety of technologies, including Java, JavaScript, React, and I am constantly expanding my skillset. Feel free to reach out for collaboration or any queries.
+            </Typography>
+          </CardContent>
+        )}
+        {showContent3 && (
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              Contact
+            </Typography>
+            <Typography variant="body1" style={{ lineHeight: '1.0' }}>
+              <p>
+                Phone: <a href={`tel:${phoneNumber}`} onClick={handlePhoneClick}>{phoneNumber}</a>
+              </p>
+              <p>
+                Email: <a href={`mailto:${email}`} onClick={handleEmailClick}>{email}</a>
+              </p>
+              <p>
+                LinkedIn: <a href={`https://www.linkedin.com/in/${linkedInId}`} onClick={handleLinkedInClick} target="_blank" rel="noopener noreferrer">
+                  {linkedInId}
+                </a>
+              </p>
+            </Typography>
+          </CardContent>
+        )}
+      </Card>
+      <Card className='showImage'>
+        {showContent2 && (
+          <div>
+            <img src={myImage} alt="Jazib Ahmad" />
+          </div>
+
+        )}
+      </Card>
+    </div >
+  );
+}
+
+export default Home;
